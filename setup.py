5fe668ebc305d00704aa098a1d6afb6cf393835e
hopdebee <helder.opdebeeck@ugent.be>
2022-11-09 22:28:40 +0100

    removed spurious code

--- setup.py
+++ setup.py
@@ -3,24 +3,22 @@ from setuptools import setup
 
 setup(
     name='bandcamp-player',
-    version='0.2.1',
+    version='0.0.0',
     packages=['bandcamp_player', 'bandcamp_parser'],
     url='https://github.com/strizhechenko/bandcamp-player',
     license='MIT',
-    author='Oleg Strizhechenko',
-    author_email='oleg.strizhechenko@gmail.com',
-    description='Utility for streaming random music from bandcamp by specified tag',
+    author='helder opdebeeck',
+    author_email='helderopdebeeck@gmail.com',
+    description='Utility for streaming random music from bandcamp by specified genre/subgenre',
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 0 - Beta',
         'Intended Audience :: End Users/Desktop',
         'Topic :: Multimedia :: Sound/Audio',
         'License :: Public Domain',
         'Programming Language :: Python :: 3.6',
     ],
     install_requires=[
-        'beautifulsoup4',
-        'requests',
-        'bandcamp-downloader'
+
     ],
     entry_points={
         'console_scripts': [
